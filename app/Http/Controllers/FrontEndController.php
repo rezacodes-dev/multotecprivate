77939feda698143c2a03e154e3ba64722b444962
Reza <syedalireza@karmicksolutions.com>
2026-05-12 18:58:09 +0530

    Push

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -3012,7 +3012,7 @@ $query = BrochureMaster::with([
         // $uniqueYear = array_unique( $yearArr );
         // $DataBag['yearList'] = $uniqueYear;
 
-        $DataBag['extraContent'] = \App\Models\Media\MediaExtraContent::where('type', '=', 'ARTICLE')->first();
+        $DataBag['extraContent'] = \App\Models\Media\MediaExtraContent::where('type', '=', 'Brochures')->first();
 
         $DataBag['page_metadata'] = $DataBag['extraContent'];
         // dd($DataBag['listData'] );
